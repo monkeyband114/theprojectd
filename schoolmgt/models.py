145f691ef3e57a54726dff21b6cc8943494fd705
Michael Voke <gadocer54@gmail.com>
2023-01-24 00:49:38 +0100

    modified:   Firststep/__pycache__/settings.cpython-310.pyc 	modified:   Firststep/__pycache__/urls.cpython-310.pyc 	modified:   Firststep/settings.py 	modified:   Firststep/urls.py 	modified:   schoolmgt/admin.py 	modified:   schoolmgt/models.py 	modified:   schoolmgt/views.py
    	Templates/
    	adminpages/
    	schoolmgt/Templates/
    	schoolmgt/__pycache__/
    	schoolmgt/migrations/0001_initial.py
    	schoolmgt/migrations/0002_alter_fees_fee.py
    	schoolmgt/migrations/__pycache__/
    	schoolmgt/urls.py
    	static/

--- schoolmgt/models.py
+++ schoolmgt/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    midle_name = models.CharField(max_length=100)
+    basic = models.CharField(max_length=200)
+    gender = models.CharField(max_length=50)
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    midle_name = models.CharField(max_length=100)
+    basic = models.CharField(max_length=200)
+    gender = models.CharField(max_length=50)
+    
+class Basic(models.Model):
+    basic_no = models.CharField(max_length=50)
+    no_students = models.IntegerField(max_length=100)
+    
+    
+    def __str__(self) -> str:
+        return self.basic_no
+    
+    
+class Fees(models.Model):
+    basic = models.ForeignKey(Basic, on_delete=models.SET_NULL, blank=True, null=True)
+    Fee = models.IntegerField(max_length=100)
+    
+
+class Attendance(models.Model):
+    basic = models.ForeignKey(Basic, on_delete=models.SET_NULL, blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
