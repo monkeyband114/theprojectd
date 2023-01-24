0d2c30eb37d96375cb403b64a8396480997ed89c
Michael Voke <gadocer54@gmail.com>
2023-01-24 02:24:20 +0100

    Changes to be committed: 	modified:   .gitignore 	modified:   Firststep/__pycache__/settings.cpython-310.pyc 	modified:   Firststep/__pycache__/urls.cpython-310.pyc 	modified:   Firststep/settings.py 	modified:   Firststep/urls.py 	new file:   Templates/main.html 	new file:   Templates/nav.html 	new file:   schoolmgt/Templates/schoolmgt/index.html 	new file:   schoolmgt/__pycache__/__init__.cpython-310.pyc 	new file:   schoolmgt/__pycache__/admin.cpython-310.pyc 	new file:   schoolmgt/__pycache__/apps.cpython-310.pyc 	new file:   schoolmgt/__pycache__/models.cpython-310.pyc 	new file:   schoolmgt/__pycache__/urls.cpython-310.pyc 	new file:   schoolmgt/__pycache__/views.cpython-310.pyc 	modified:   schoolmgt/admin.py 	new file:   schoolmgt/migrations/0001_initial.py 	new file:   schoolmgt/migrations/0002_alter_fees_fee.py 	new file:   schoolmgt/migrations/0003_subjectb_basic_subjects.py 	new file:   schoolmgt/migrations/0004_remove_teacher_basic_teacher_phone.py 	new file:   schoolmgt/migrations/0005_teacher_basic.py 	new file:   schoolmgt/migrations/0006_student_parents_name_alter_student_gender.py 	new file:   schoolmgt/migrations/0007_alter_teacher_gender.py 	new file:   schoolmgt/migrations/0008_alter_teacher_basic.py 	new file:   schoolmgt/migrations/0009_attendance_student.py 	new file:   schoolmgt/migrations/0010_alter_student_basic.py 	new file:   schoolmgt/migrations/0011_student_user_teacher_user.py 	new file:   schoolmgt/migrations/__pycache__/0001_initial.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0002_alter_fees_fee.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0003_subjectb_basic_subjects.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0004_remove_teacher_basic_teacher_phone.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0005_teacher_basic.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0006_student_parents_name_alter_student_gender.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0007_alter_teacher_gender.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0008_alter_teacher_basic.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0009_attendance_student.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0010_alter_student_basic.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/0011_student_user_teacher_user.cpython-310.pyc 	new file:   schoolmgt/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   schoolmgt/models.py 	new file:   schoolmgt/urls.py 	modified:   schoolmgt/views.py

--- schoolmgt/admin.py
+++ schoolmgt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Basic)
+admin.site.register(Fees)
+admin.site.register(Attendance)
+admin.site.register(SubjectB)
--- schoolmgt/models.py
+++ schoolmgt/models.py
@@ -1,34 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    midle_name = models.CharField(max_length=100)
-    basic = models.CharField(max_length=200)
-    gender = models.CharField(max_length=50)
-
-class Teacher(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    midle_name = models.CharField(max_length=100)
-    basic = models.CharField(max_length=200)
-    gender = models.CharField(max_length=50)
+class SubjectB(models.Model):
+    name = models.CharField(max_length=200)
     
+    def __str__(self) -> str:
+        return self.name
+
 class Basic(models.Model):
     basic_no = models.CharField(max_length=50)
-    no_students = models.IntegerField(max_length=100)
-    
+    subjects = models.ManyToManyField(SubjectB, blank=True, null=True)
     
     def __str__(self) -> str:
         return self.basic_no
     
+gen_chices = (
+    ("male", "male"),
+    ("female", "female")
+)
+
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    midle_name = models.CharField(max_length=100)
+    basic = models.OneToOneField(Basic, on_delete=models.SET_NULL, blank=True, null=True )
+    gender = models.CharField(max_length=50, choices=gen_chices)
+    parentS_name = models.CharField(max_length=50, default='john')
+    
+    def __str__(self) -> str:
+        return self.first_name
+    
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    midle_name = models.CharField(max_length=100)
+    basic = models.OneToOneField(Basic, on_delete=models.SET_NULL, blank=True, null=True )
+    gender = models.CharField(max_length=50, choices=gen_chices)
+    phone = models.CharField(max_length=16)
+    
+    def __str__(self) -> str:
+        return self.first_name
+
+    
+
     
 class Fees(models.Model):
     basic = models.ForeignKey(Basic, on_delete=models.SET_NULL, blank=True, null=True)
-    Fee = models.IntegerField(max_length=100)
+    Fee = models.IntegerField()
     
 
 class Attendance(models.Model):
     basic = models.ForeignKey(Basic, on_delete=models.SET_NULL, blank=True, null=True)
     date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, blank=True, null=True)
