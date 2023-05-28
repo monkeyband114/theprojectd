a65d3ae9ff7b2fd1dd73e7542d522d99b769821f
Michael Voke <gadocer54@gmail.com>
2023-05-28 19:34:47 +0100

    Changes to be committed: 	modified:   Firststep/__pycache__/settings.cpython-310.pyc 	modified:   Firststep/settings.py

--- Firststep/settings.py
+++ Firststep/settings.py
@@ -15,9 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@tei-0p91)rtv7)q&n%aw7ihxvch0+0)_cj0mnrd75qh!6ivaw'
