7f1c42f7db2fa26e471ab8daa4977427a33f4e70
Michael Voke <gadocer54@gmail.com>
2023-06-10 19:34:52 +0100

    first commit

--- Firststep/asgi.py
+++ Firststep/asgi.py
@@ -13,4 +13,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Firststep.settings')
 
-application = get_asgi_application()
+application = get_asgi_application()  
+
+# jeremmy renner 
+
